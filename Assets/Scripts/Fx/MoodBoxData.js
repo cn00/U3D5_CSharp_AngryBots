9843e469c5ce7007e54dffe4c5148b57090ab086
cnavy <cool_navy@126.com>
2015-10-13 14:13:45 +0800

    add missing js

---
+++ Assets/Scripts/Fx/MoodBoxData.js
@@ -0,0 +1,12 @@
+#pragma strict
+
+public class MoodBoxData_js {
+//var MoodBoxData = {
+	var noiseAmount : float = 0.375f;
+	var colorMixBlend : float = 0.0f;
+	var colorMix : Color = Color.green;
+	var fogY : float = -10.0;
+	var fogColor : Color = Color.black;
+	var adventureString : System.String = "";
+	var outside : boolean = false;
+};
