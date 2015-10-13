eec41f739081977f6324c7e23b998681de43972f
cnavy <cool_navy@126.com>
2015-10-13 14:24:42 +0800

    add face to camera health label

---
+++ Assets/Scripts/FaceToCamera.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FaceToCamera : MonoBehaviour {
+	public Vector3 cameraDirection;
+	public Camera camera;
+
+	void Awake(){
+		camera = Camera.main;
+	}
+
+	void Update()
+	{
+//		cameraDirection = Camera.main.transform.forward;
+		if (camera) {
+			cameraDirection = camera.transform.forward;
+//			cameraDirection.y = 0;
+			this.transform.rotation = Quaternion.LookRotation (cameraDirection);
+		}
+	}
+
+}
