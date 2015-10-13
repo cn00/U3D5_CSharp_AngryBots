d6f7827d2a6e155ce817acfdcc5ba8efa9b32520
cool_navy <cool_navy@126.com>
2015-10-13 14:58:22 +0800

    add a ban to add health

---
+++ Assets/Scripts/UI/AddHealthButton.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class MyButton : MonoBehaviour {
+	public Health health;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	static int i = 0;
+	public void OnClick()
+	{
+		++i;
+		Debug.Log ("Add Health clicked: "+i);
+		Text text = GetComponentInChildren<Text> ();
+		text.text = ( "Add Health:" + i);
+		health.SetHealth( health.health + 10);
+	}
+}
