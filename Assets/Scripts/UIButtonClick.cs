43dbfc8425a655e10ca9cdba819878d440217f49
cool_navy <cool_navy@126.com>
2015-10-16 13:53:08 +0800

    rename

---
+++ Assets/Scripts/UIButtonClick.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class UIButtonClick : MonoBehaviour {
+	static int i = 0;
+	public Health health;
+
+	public void OnAddHealth10()
+	{
+		++i;
+		foreach (GameObject player in GameObject.FindGameObjectsWithTag("Player")) {
+			if (player.GetComponent<PlayerScript> ().isLocalPlayer) {
+				health = player.GetComponent<Health>();
+				break;
+			}
+		}
+		if (health) {
+			Debug.Log ("Add Health clicked: " + i);
+			Text text = GetComponentInChildren<Text> ();
+			text.text = ("Add Health:" + i);
+			health.SetHealth (health.health + 10);
+		}
+	}
+
+	public void OnLogout(){
+		Network.Disconnect(200);
+	}
+}
